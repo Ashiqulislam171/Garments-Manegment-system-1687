fd416161e1fc43d1d27ee686f5dab333b4a0dada
Ashiqulislam171 <islam2305101687@diu.edu.bd>
2024-11-14 21:25:40 +0600

    Garments Class

---
+++ src/Garment.java
@@ -0,0 +1,29 @@
+public class Garment {
+    private String id;
+    private String name;
+    private String description;
+    private String size;
+    private String color;
+    private double price;
+    private int stockQuantity;
+
+    public Garment(String id, String name, String description, String size, String color, double price, int stockQuantity) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.size = size;
+        this.color = color;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public void updateStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public double calculateDiscountPrice(double discountPercentage) {
+        return this.price * (1 - discountPercentage / 100);
+    }
+
+    // Getters and setters can be added here
+}
