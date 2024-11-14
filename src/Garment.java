5d69a9001d74f4f81d72c247908021b1b60b5609
Ashiqulislam171 <islam2305101687@diu.edu.bd>
2024-11-14 22:32:07 +0600

    Update Garment.java

--- src/Garment.java
+++ src/Garment.java
@@ -17,13 +17,42 @@ public class Garment {
         this.stockQuantity = stockQuantity;
     }
 
+    // Method to update stock quantity
     public void updateStock(int quantity) {
         this.stockQuantity += quantity;
     }
 
+    // Method to calculate discounted price
     public double calculateDiscountPrice(double discountPercentage) {
         return this.price * (1 - discountPercentage / 100);
     }
 
-    
+    // Getter methods
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
 }
