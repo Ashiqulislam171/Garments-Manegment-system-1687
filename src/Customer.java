f2df8bfea476465223ae36b30fb84c2dae9878db
Ashiqulislam171 <islam2305101687@diu.edu.bd>
2024-11-14 22:31:07 +0600

    Create Customer.java

---
+++ src/Customer.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+    private String customerId;
+    private String name;
+    private String email;
+    private String phone;
+    private List<Order> orders;
+
+    public Customer(String customerId, String name, String email, String phone) {
+        this.customerId = customerId;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.orders = new ArrayList<>();
+    }
+
+    public void placeOrder(Order order) {
+        orders.add(order);
+    }
+
+    public List<Order> viewOrders() {
+        return orders;
+    }
+
+    // Getters and setters can be added here
+}
